f38988b110c8df269a35686d89c48e48cc147032
superavatar888 <superavatar888@gmail.com>
2025-12-21 01:43:36 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-    },
-    
-    distDir: "out",
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
+  // output: 'export',  <-- 删除或注释这一行
+  
+  // 如果你需要允许跨域图片，保留 images 配置，否则可以忽略
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
